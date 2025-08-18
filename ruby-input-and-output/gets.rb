3bfc460ff11de9639f60b16cf81d994c1ab795c2
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2025-08-18 10:48:55 -0300

    test gets, puts, p, etc.

---
+++ ruby-input-and-output/gets.rb
@@ -0,0 +1,20 @@
+myVar = gets
+
+p "You wrote #{myVar}"
+
+myVar = gets.chomp
+
+p "and then you wrote #{myVar}"
+
+myVar = gets
+
+p "and finally you wrote #{myVar}"
+
+=begin
+testing out some comments
+=end
+
+# This
+# is
+# pretty
+# cool
